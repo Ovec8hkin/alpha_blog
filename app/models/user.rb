34e4ec09593a3a6308a11caaca2a13e0c2d2afc7
Joshua Zahner <joshuazahner@gmail.com>
2017-07-10 23:29:11 -0500

    Added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
 
+  validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 15}
+
+  validates :email, presence: true, uniqueness: true, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i,
+                                                               message: "Not a valid email format" }
 end

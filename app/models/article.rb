97bfd3a0a8190ea883f7828f19dc271203c9738e
Joshua Zahner <joshuazahner@gmail.com>
2017-07-10 23:52:57 -0500

    Added associations between user and articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ActiveRecord::Base
 
+  belongs_to :user
+
   validates :title, presence: true, length: {minimum: 1, maximum: 50}
   validates :description, presence: true, length: {minimum: 1, maximum: 100000}
 

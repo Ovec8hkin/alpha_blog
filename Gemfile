54575abe9b63f2c929d68193612b9025199e793c
Joshua Zahner <joshuazahner@gmail.com>
2017-07-12 00:18:59 -0500

    Added secure_passwords to user

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'rails', '~> 5.0.4'
 
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
+# Use bcrypt to create secure passwords
+gem 'bcrypt', '~> 3.1.7'
 # Use SCSS for stylesheets
 gem 'bootstrap-sass', '~> 3.3.5'
 gem 'sass-rails', '~> 5.0'
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 15}
   validates :email, presence: true, uniqueness: true, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i,
                                                                message: "Not a valid email format" }
-  validates :user_id, presence: true
+  has_secure_password
 
 end

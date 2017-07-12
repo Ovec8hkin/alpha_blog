043467f7d8867bb7c4f6710aac699c2fb5c5c580
Joshua Zahner <joshuazahner@gmail.com>
2017-07-12 15:40:45 -0500

    Added user info to article show view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
 
-  def gravatar_for(user, options = {size: 80})
+  def gravatar_for(user, options = {size: 80, classes: 'img-circle'})
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{options[:size]}"
-    image_tag(url, class: 'img-circle')
+    image_tag(url, class: options[:classes])
   end
 
 end

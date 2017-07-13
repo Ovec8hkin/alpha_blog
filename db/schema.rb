b8cbb9affe7a7729f815b4afeeb0aa5b01d557fb
Joshua Zahner <joshuazahner@gmail.com>
2017-07-12 22:11:10 -0500

    Added bio data to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170712051434) do
+ActiveRecord::Schema.define(version: 20170713024427) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20170712051434) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "bio"
   end
 
 end

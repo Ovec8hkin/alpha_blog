ee81478deb5341720b0dfbbfdc7c6a20a8e02088
Joshua Zahner <joshuazahner@gmail.com>
2017-07-13 20:10:23 -0500

    Restricted actions by logged in state in views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_action :setUser, only: [:edit, :update, :show, :destroy]
+  before_action :set_user, only: [:edit, :update, :show, :destroy]
 
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -49,7 +49,7 @@ class UsersController < ApplicationController
     params.require(:user).permit(:username, :email, :password, :bio)
   end
 
-  def setUser
+  def set_user
     @user = User.find(params[:id])
   end
 

8b76c7c2847c4b08c76a77e4a3aff4633e7e2739
Joshua Zahner <joshuazahner@gmail.com>
2017-07-18 19:30:20 -0500

    Added category descriptions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -50,7 +50,7 @@ class CategoriesController < ApplicationController
   private
 
   def category_params
-    params.require(:category).permit(:name)
+    params.require(:category).permit(:name, :description)
   end
 
   def set_category
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170718201932) do
+ActiveRecord::Schema.define(version: 20170719002629) do
 
   create_table "article_categories", force: :cascade do |t|
     t.integer "article_id"
@@ -27,8 +27,9 @@ ActiveRecord::Schema.define(version: 20170718201932) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
   end
 
   create_table "users", force: :cascade do |t|

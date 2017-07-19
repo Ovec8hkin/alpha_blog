2197eb43d5e317bf2bb8572dd6f904c18f60b3ac
Joshua Zahner <joshuazahner@gmail.com>
2017-07-18 19:26:11 -0500

    Added edit and delete actions and view to categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,7 @@
 
 class CategoriesController < ApplicationController
 
+  before_action :set_category, except: [:create, :new, :index]
   before_action :require_admin, except: [:index, :show]
 
   def index
@@ -23,15 +24,37 @@ class CategoriesController < ApplicationController
 
   end
 
+  def edit
+
+  end
+
+  def update
+    if @category.update(category_params)
+      flash[:success] = "Category was succesfully updated"
+      redirect_to category_path(@category)
+    else
+      render 'edit'
+    end
+  end
+
   def show
-    @category = Category.find(params[:id])
     @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
+  end
+
+  def destroy
+    @category.destroy
+    flash[:danger] = "Category deleted"
+    redirect_to categories_path
   end
 
   private
 
   def category_params
     params.require(:category).permit(:name)
+  end
+
+  def set_category
+    @category = Category.find(params[:id])
   end
 
   def require_admin
